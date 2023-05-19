63350574984312e5a2851b11d88e239263aafcf6
sidnnov <sidnnov@gmail.com>
2023-05-19 15:45:07 +0100

    Add auth for delete, update, view, create

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,23 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import View
-from task_manager.tasks.forms import TasksForm
-from task_manager.tasks.models import Tasks
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.utils.translation import gettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+
+from task_manager.tasks.forms import TasksForm
+from task_manager.tasks.models import Tasks
 
 
 # Create your views here.
-class TasksView(View):
+class TaskLoginMixin(LoginRequiredMixin):
+    not_auth_message = _("You are not authorized! Please log in.")
 
+    def handle_no_permission(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, self.not_auth_message)
+            return redirect("login")
+
+
+class TasksView(TaskLoginMixin, View):
     def get(self, request):
         tasks = Tasks.objects.all()
-        return render(request, "tasks/tasks.html", context={
-            "tasks": tasks})
+        return render(request, "tasks/tasks.html", context={"tasks": tasks})
 
 
-class CreateTaskView(SuccessMessageMixin, CreateView):
+class CreateTaskView(TaskLoginMixin, SuccessMessageMixin, CreateView):
     model = Tasks
     form_class = TasksForm
     template_name = "tasks/create.html"
@@ -30,11 +40,10 @@ class CreateTaskView(SuccessMessageMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.save()
         return super().form_valid(form)
 
 
-class UpdateTaskView(SuccessMessageMixin, UpdateView):
+class UpdateTaskView(TaskLoginMixin, SuccessMessageMixin, UpdateView):
     model = Tasks
     form_class = TasksForm
     template_name = "tasks/create.html"
@@ -45,8 +54,22 @@ class UpdateTaskView(SuccessMessageMixin, UpdateView):
     }
 
 
-class DeleteTaskView(SuccessMessageMixin, DeleteView):
+class DeleteTaskView(
+    TaskLoginMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView
+):
     model = Tasks
     template_name = "tasks/delete.html"
     success_url = reverse_lazy("tasks")
     success_message = _("Task successfully deleted")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, self.not_auth_message)
+            return redirect("login")
+        messages.error(self.request,
+                       _("The task can be deleted only by its author"))
+        return redirect(self.success_url)
