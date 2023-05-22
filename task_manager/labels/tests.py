36409b8ccab035cd4f4e68a86713ed9b47ae3ac4
sidnnov <sidnnov@gmail.com>
2023-05-22 18:09:47 +0100

    Add test

--- task_manager/labels/tests.py
+++ task_manager/labels/tests.py
@@ -25,10 +25,16 @@ class LabelsTestCase(TestCase):
         self.assertEqual(labels.name, self.label_data["name"])
         self.assertRedirects(response, reverse_lazy("labels"))
 
-    def test_label_update(self):
+    def test_update_label(self):
         url = reverse("update_label", kwargs={"pk": self.label.pk})
         response = self.client.post(url, self.label_data)
         update_label = Labels.objects.get(pk=self.label.pk)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(update_label.name, self.label_data["name"])
         self.assertRedirects(response, reverse_lazy("labels"))
+
+    def test_delete_label(self):
+        url = reverse("delete_label", args=[self.label.pk])
+        response = self.client.post(url, follow=True)
+        self.assertRedirects(response, reverse_lazy("labels"))
+        self.assertFalse(Labels.objects.filter(pk=self.label.pk).exists())
