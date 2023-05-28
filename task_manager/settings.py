6e672c143b16a934acebea4ecb47c3f2bd742bc2
sidnnov <sidnnov@gmail.com>
2023-05-28 23:00:23 +0100

    test 2

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -35,14 +35,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.railway.app",
 ]
 
-# ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 
-# ROLLBAR = {
-#     'access_token': ACCESS_TOKEN,
-#     'environment': 'development' if DEBUG else 'production',
-#     'code_version': '1.0',
-#     'root': BASE_DIR,
-# }
 
 ALLOWED_HOSTS = [
     "task-manager-project-52.up.railway.app",
@@ -80,8 +74,15 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "rollbar.contrib.django.middleware.RollbarNotifierMiddleware",
+    "rollbar.contrib.django.middleware.RollbarNotifierMiddleware",
 ]
+
+ROLLBAR = {
+    'access_token': ACCESS_TOKEN,
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
 
 ROOT_URLCONF = "task_manager.urls"
 
