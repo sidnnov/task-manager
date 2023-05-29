5874e1f094d8ebf27099689f48fbed50be4d84cb
sidnnov <sidnnov@gmail.com>
2023-05-30 00:35:50 +0100

    Update /tasks/views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -6,8 +6,11 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
 
 from django_filters.views import FilterView
+from task_manager.labels.models import Labels
+from task_manager.statuses.models import Statuses
 from task_manager.tasks.filters import TaskFilter
 from task_manager.tasks.models import Tasks
+from task_manager.users.models import CustomUser
 from task_manager.utilities import AuthorizationMixin, UserPermissionMixin
 
 
@@ -37,6 +40,13 @@ class CreateTaskView(AuthorizationMixin, SuccessMessageMixin, CreateView):
         "table_name": _("Create task"),
         "button_name": _("Create"),
     }
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields["status"].queryset = Statuses.objects.order_by('id')
+        form.fields["executor"].queryset = CustomUser.objects.order_by("id")
+        form.fields["labels"].queryset = Labels.objects.order_by('id')
+        return form
 
     def form_valid(self, form):
         form.instance.author = self.request.user
